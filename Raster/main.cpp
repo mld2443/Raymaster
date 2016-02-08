8308db9952317e83a6318e380909b83ae085e501
Matthew Dillard <7395051@gmail.com>
2016-02-07 21:40:52 -0600

    Initial Commit

---
+++ Raster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raster
+//
+//  Created by Matthew Dillard on 2/7/16.
+//  Copyright © 2016 Matthew Dillard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
